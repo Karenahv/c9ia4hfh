265cb0f3f399ada47c81497687b7f00e3952ecb2
karenahv <karenahv@gmail.com>
2019-04-22 18:54:58 -0500

    edit and eliminate

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,25 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+  @room = Room.find(params[:id])
+  end
+
+def update
+  @room = Room.find(params[:id])
+  if @room.update_attributes(room_params)
+    flash[:notice] = 'Profile was successfully updated.'
+    redirect_to rooms_path
+  else
+    render "edit"
+  end
+end
+def destroy
+  id = params[:id]
+  @room = Room.find(id)
+  @room.destroy
+  redirect_to "/rooms"
+end
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
